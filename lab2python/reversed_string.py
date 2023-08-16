e78cb67f2678b508d8da04597353874d40d24c30
Mostafa-Eid2 <mmeun3@gmail.com>
2023-08-16 15:06:37 +0300

    reversed string exercise and updated folder

---
+++ lab2python/reversed_string.py
@@ -0,0 +1,5 @@
+print("This program is used to display an inputted string reversed")
+def rev_string():
+    inputstr = input("please input a string to be reversed:  ")
+    print(inputstr[::-1])
+rev_string()
