116e2133f2a3cd0b85e0f57e4447fa1c6367c771
Mostafa-Eid2 <mmeun3@gmail.com>
2023-08-16 18:13:31 +0300

    update on ensure name and email

--- lab2python/ensure_name_entered.py
+++ lab2python/ensure_name_entered.py
@@ -1,25 +1,42 @@
 print("..........................................................................")
 print("A program to ensure the user entered his name nothing else")
-print("..........................................")
+print("..........................................................................")
+#فضلت شوية ازود مجموعة خواص اضافية وربما اضافة المزيد لاحقا
 def entername():
+    while True:
         name = input("Please, enter your name, nothing else: ")
-        if name.isalpha():
-            print("Valid Named; stored successfully")
-        else:
-            print("Inalid Named; please enter only letters...")
-entername()
+        if name.lower() == "exit":
+            print("Exiting the program.")
+            raise SystemExit  # Raise an exception to terminate the program
+        elif name.isalpha():
+            print("Valid Name; stored successfully")
+            return name
+
+name = entername()
 
 def validate_email(email):
-    if "@" in email and "." in email:
-        at_index = email.index("@")
-        dot_index = email.index(".")
-        if at_index < dot_index:
-            print("Valid Email; stored successfully")
+    while True:
+        email = input("Please, enter your email address: ")
+        if email.lower() == "exit":
+            print("Exiting the program.")
+            raise SystemExit  # Raise an exception to terminate the program
+        if "@" in email and "." in email:
+            at_index = email.index("@")
+            dot_index = email.index(".")
+            if at_index < dot_index:
+                print("Valid Email; stored successfully")
+                return email
+            else:
+                print("Invalid Email; please enter a valid email address")
         else:
             print("Invalid Email; please enter a valid email address")
-    else:
-        print("Invalid Email; please enter a valid email address")
 
-while True:
-    email = input("Please, enter your email address: ")
-    validate_email(email)
+email = input("Please, enter your email address: ")
+if email.lower() == "exit":
+    print("Exiting the program.")
+    raise SystemExit  # Raise an exception to terminate the program
+else:
+    email = validate_email(email)
+
+print("Name:", name)
+print("Email:", email)
