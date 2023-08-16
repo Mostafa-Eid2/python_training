c5015b1c55191adaff99d4c36a25cb7a9aa3f1a8
Mostafa-Eid2 <mmeun3@gmail.com>
2023-08-16 17:42:48 +0300

    essure name updated

--- lab2python/ensure_name_entered.py
+++ lab2python/ensure_name_entered.py
@@ -1,11 +1,10 @@
 print("..........................................................................")
 print("A program to ensure the user entered his name nothing else")
 print("..........................................")
-while True
 def entername():
-    name = input("Please, enter your name, nothing else: ")
-    if name.isalpha():
-        print("Valid Named; stored successfully")
-    else:
-        print("Inalid Named; please enter only letters...")
+        name = input("Please, enter your name, nothing else: ")
+        if name.isalpha():
+            print("Valid Named; stored successfully")
+        else:
+            print("Inalid Named; please enter only letters...")
 entername()
