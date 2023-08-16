3212181779edb3d89448fb7b7b58f6fdf1869421
Mostafa-Eid2 <mmeun3@gmail.com>
2023-08-16 17:36:52 +0300

    update

--- Lab1python/build_a_mario_pyramid.py
+++ Lab1python/build_a_mario_pyramid.py
@@ -1,7 +1,5 @@
-# Step 1: User Input
 num = int(input("Enter a number: "))
 
-# Step 2: Build Mario Pyramid
 for i in range(1, num + 1):
     spaces = ' ' * (num - i)
     stars = '*' * i
--- build_a_mario_pyramid.py
+++
@@ -1,8 +0,0 @@
-# Step 1: User Input
-num = int(input("Enter a number: "))
-
-# Step 2: Build Mario Pyramid
-for i in range(1, num + 1):
-    spaces = ' ' * (num - i)
-    stars = '*' * i
-    print(spaces + stars)
